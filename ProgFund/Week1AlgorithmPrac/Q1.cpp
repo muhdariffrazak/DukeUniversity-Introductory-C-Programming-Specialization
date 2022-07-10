908e8ff5ec5b269a2add8e0174c1a177915592b3
muhdariffrazak <108445982+muhdariffrazak@users.noreply.github.com>
2022-07-10 18:08:45 +0800

    Week1&2_codes
    Added week 1 & 2 codes. Ignored files using git ignore

---
+++ ProgFund/Week1AlgorithmPrac/Q1.cpp
@@ -0,0 +1,17 @@
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int N;
+    cout << "Type in the number of N:";
+    cin >> N;
+    for (int i = 0; i < (2 * N); i++)
+        if (i < N) {
+            cout << 2 * i << endl;
+        }
+        else {
+            cout << i<<endl;
+        }
+}
