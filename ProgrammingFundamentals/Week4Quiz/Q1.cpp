d266a67ba59647e370e2f398e402676063fcf8ba
muhdariffrazak <108445982+muhdariffrazak@users.noreply.github.com>
2022-07-21 00:17:40 +0800

    Added code answer for week 4
    Added answer for week 4's algorithm in code

--- ProgrammingFundamentals/Week4Quiz/Q1.cpp
+++ ProgrammingFundamentals/Week4Quiz/Q1.cpp
@@ -1,3 +1,52 @@
 #include<iostream>  
 using namespace std;
-int main (){}
+
+void InputData();
+
+int main(){
+    double * Array; //point to the location of array
+    cout<<"Enter number of Data: ";
+    int Size =0;
+    cin >> Size;
+    Array = new double[Size];
+    for(int i = 0; i < Size; i++){
+        cin >> Array[i];}
+    //  print array elements
+    cout << "The data are: ";
+    for (int n = 0; n < Size; ++n) {
+    cout << Array[n] << "  "; }
+    // sorting algorithm
+    int a,b,c;
+    double temp;
+    for(a=0;a < Size;a++){
+        for(b=a+1;b< Size; b++){
+            if(Array[a] > Array[b]) {
+                 temp = Array[b];
+                 Array[b] = Array[a];
+                 Array[a] = temp;
+                 } 
+            }
+         }
+    cout <<"/nSorted Element List ...\n";
+    for(c = 0; c<Size; c++) {
+    cout <<Array[c]<<"\t";
+    }
+    return 0; 
+    }
+
+void InputData(){
+    double * Array; //point to the location of array
+    cout<<"Enter number of Data: ";
+    int Size =0;
+    cin >> Size;
+    Array = new double[Size];
+    for(int i = 0; i < Size; i++){
+        cin >> Array[i];}
+    //  print array elements
+    cout << "The data are: ";
+    for (int n = 0; n < Size; ++n) {
+    cout << Array[n] << "  "; }
+};
+
+
+
